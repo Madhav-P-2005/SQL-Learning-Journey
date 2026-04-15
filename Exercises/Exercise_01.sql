e18f70d73e793c97a224e752695c2146f0ce6f07
Madhav P <163096888+Madhav-P-2005@users.noreply.github.com>
2026-04-15 19:10:48 +0530

    Day 15 Practiced

---
+++ Exercises/Exercise_01.sql
@@ -0,0 +1,11 @@
+/*
+   1. Create a Database ‘Classroom’
+
+   2. Create a table named ‘Science_class’ with the following properties • 3 Cloumns (Enrollment_no INT, Name VARCHAR, Science_Marks INT)
+*/
+
+--- Ans 1 --- 
+Create Database Classroom;
+
+--- Ans 2 --- 
+Create table Science_Class(Enrollment_no INT, Name VARCHAR, Science_class INT);
---
+++ Exercises/Intro.sql
@@ -0,0 +1,8 @@
+--- Creation of the Database with Columns --- 
+
+create table Customer_Table(Cust_id int, First_Name varchar,Last_Name varchar,Age int,Email_Id varchar);
+
+
+---- Desc Database ---
+
+Select * from Customer_Table;
