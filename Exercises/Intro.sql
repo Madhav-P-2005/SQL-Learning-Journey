4d4b56862c14761b1aaa0ef8aa297a4f4badf497
Madhav P <163096888+Madhav-P-2005@users.noreply.github.com>
2026-04-17 17:01:22 +0530

    Day 17-04-2026  Practiced

--- Exercises/Intro.sql
+++
@@ -1,8 +0,0 @@
---- Creation of the Database with Columns --- 
-
-create table Customer_Table(Cust_id int, First_Name varchar,Last_Name varchar,Age int,Email_Id varchar);
-
-
----- Desc Database ---
-
-Select * from Customer_Table;
