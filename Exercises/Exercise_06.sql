d73bd3a353e56ccacb56ba969ecd888c20974bd3
Madhav P <163096888+Madhav-P-2005@users.noreply.github.com>
2026-04-24 23:34:34 +0530

    Exercise 6 Solved

--- Exercises/Exercise_06.sql
+++ Exercises/Exercise_06.sql
@@ -12,3 +12,22 @@ c. Get the list of customers whose last name contains only 4 characters
 using LIKE
 
 */
+
+select * from Sales;
+
+select * from Customer;
+
+select * from Product;
+
+
+--- Ans 1 ---
+
+Select distinct city from Customer where region in ('North','East');
+
+--- Ans 2 ---
+
+Select * from Sales where Sales between 100 and 500;
+
+--- Ans 3 ---
+
+Select Customer_Name from Customer where Customer_Name LIKE '%____';
