e9c94df7f30b419df694d7e22f11292e666b4c47
Madhav P <163096888+Madhav-P-2005@users.noreply.github.com>
2026-04-24 19:09:20 +0530

    Exercise 6 Solved

---
+++ Exercises/Exercise_06.sql
@@ -0,0 +1,14 @@
+/* 
+
+1. In the database Supermart_DB, find the following :- 
+
+a. Get the list of all cities where the region is South or east without any 
+duplicates using IN statement.
+
+b. Get the list of all orders where the ‘sales’ value is between 100 and 
+500 using the BETWEEN operator.
+
+c. Get the list of customers whose last name contains only 4 characters 
+using LIKE
+
+*/
