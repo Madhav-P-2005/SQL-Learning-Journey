694eea4b02395dbb4b186e6fcc67181353696981
Madhav P <163096888+Madhav-P-2005@users.noreply.github.com>
2026-09-05 23:19:08 +0530

    Exercise Updated

---
+++ Exercises/Exercise_12.sql
@@ -0,0 +1,12 @@
+/* 
+
+1) Create a View which contains order_line, Product_id, sales and discount 
+value of the first order date in the sales table and name it as 
+“Daily_Billing” 
+
+2)  Delete this View
+
+*/
+
+
+--- Ans 1 ---
