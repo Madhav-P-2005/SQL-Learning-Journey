0491b7cef1b86923dbd5209ef76dc6412a41a134
Madhav P <163096888+Madhav-P-2005@users.noreply.github.com>
2026-09-06 02:47:38 +0530

    Exercise Updated

--- Exercises/Exercise_12.sql
+++ Exercises/Exercise_12.sql
@@ -8,5 +8,60 @@ value of the first order date in the sales table and name it as
 
 */
 
-
 --- Ans 1 ---
+Select * from sales;
+
+-- My Way --
+CREATE VIEW Daily_Billing AS
+SELECT
+    order_line,
+    product_id,
+    sales,
+    discount
+FROM sales
+ORDER BY order_date ASC
+LIMIT 1;
+
+-- sir's way --
+CREATE VIEW Daily_Billing AS
+SELECT
+    order_line,
+    product_id,
+    sales,
+    discount
+FROM sales
+WHERE order_date IN (
+    SELECT MAX(order_date)
+    FROM sales
+);
+
+-- LLM Ans --- 
+Create view Daily_Billing as 
+Select order_line,
+       product_id,
+	   sales,
+	   discount
+from sales 
+where order_date in (
+       select min(order_date) 
+	   from sales
+);
+
+-- or --
+
+CREATE VIEW "Daily_Billing" AS
+SELECT 
+    order_line,
+    product_id,
+    sales,
+    discount
+FROM sales
+WHERE order_date = (
+    SELECT MIN(order_date) 
+    FROM sales
+);
+
+Select * from Daily_Billing;
+
+--- Ans 2 ---
+drop view Daily_Billing;
