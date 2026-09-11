38025067d7685a133137f2c82d3a83e025993290
Madhav P <163096888+Madhav-P-2005@users.noreply.github.com>
2026-09-11 23:22:35 +0530

    Date-Time Functions Updated

---
+++ 16_Date-Time-Functions/date-time-functions.sql
@@ -0,0 +1,8 @@
+--- CURRENT_DATE :- This function returns current date
+
+
+--- CURRENT_TIME :- This function returns the current time with the time zone.
+
+
+--- CURRENT_TIMESTAMP :- This function returns the current date and time with the time zone. 
+
