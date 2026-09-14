eef41a5529298adce5806e6b918d117aa5d5c0e0
Madhav P <163096888+Madhav-P-2005@users.noreply.github.com>
2026-09-14 20:56:02 +0530

    Pattern(String)  Matching Completed

--- 17_Pattern_Matching/pattern-matching.sql
+++ 17_Pattern_Matching/pattern-matching.sql
@@ -14,3 +14,20 @@ Select * from customer;
 
 Select first_name , last_name from customer_table where first_name LIKE 'Jas_n';
 
+--- Using regular expressions --- 
+
+Select * from customer;
+
+Select * from customer where customer_name~* '^a+[a-z\s]+$';
+
+Select * from customer where customer_name~* '^(a|b|c|d)+[a-z\$$]+';
+
+Select * from customer where customer_name~* '^(a|b|c|d)[a-z]{3}\s[a-z]{4}$';
+
+create table users(id serial primary key, name character varying);
+
+insert into users (name) VALUES ('Alex'), ('Jon Snow'), ('Christopher'), ('Arya'),('Sandip Debnath'), ('Lakshmi'),('alex@gmail.com'),('@sandip5004'), ('lakshmi@gmail.com');
+
+Select * from users;
+
+Select * from users where name~*'[a-z0-9\.\-\_]+@[a-z0-9\-]+\.[a-z]{2,5}';
