1eb909aa28d892551010feccedb5c94506ed202c
Madhav P <163096888+Madhav-P-2005@users.noreply.github.com>
2026-09-15 23:17:12 +0530

    Windows Functions Completed

---
+++ 18_Windows_Functions/windows-functions.sql
@@ -0,0 +1,11 @@
+/* 
+
+ 1) Windows function provide the ability to perform calculations across sets of rows that are related to the current query row .
+
+ 2) Window functions compute their result based on a sliding window frame, a set of rows that are somehow related to the current row.
+
+ 3) Unlike Aggregate functions a window function does not cause rows to become grouped into a single output row - the rows retain their separate identities. 
+
+ 4) Behind the scenes , the window function can access more than just the current row of the query result. 
+
+*/
