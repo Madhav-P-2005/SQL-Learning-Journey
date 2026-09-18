b3510711201beff2dfe3dbcbe890621eb722a21f
Madhav P <163096888+Madhav-P-2005@users.noreply.github.com>
2026-09-18 23:26:54 +0530

    Window Functions Completed

--- 18_Windows_Functions/windows-functions.sql
+++ 18_Windows_Functions/windows-functions.sql
@@ -37,3 +37,4 @@ from customer_order;
 --- Top 3 customers from each state ---
 Select * from (Select customer_id , customer_name , state , order_num , ROW_NUMBER() over (partition by state order by order_num desc) as row_number
 from customer_order) as a where a.row_number <=3;
+
