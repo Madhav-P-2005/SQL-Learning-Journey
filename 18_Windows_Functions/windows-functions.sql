c54b1ba079e3138653890896b4d7da1ca580d8c1
Madhav P <163096888+Madhav-P-2005@users.noreply.github.com>
2026-09-19 23:25:48 +0530

    Window Functions Completed

--- 18_Windows_Functions/windows-functions.sql
+++ 18_Windows_Functions/windows-functions.sql
@@ -8,11 +8,14 @@
 
  4) Behind the scenes , the window function can access more than just the current row of the query result. 
 
+
 */
 
 select * from customer limit 10;
 
 Select * from sales limit 10;
+
+--- Row Number :- Unique number for each row within partition, with different numbers for tied values 
 
 Select a.*, b.order_num , b.sales_total, b.quantity_total, b.profit_total
 from customer as a
@@ -38,4 +41,7 @@ from customer_order;
 Select * from (Select customer_id , customer_name , state , order_num , ROW_NUMBER() over (partition by state order by order_num desc) as row_number
 from customer_order) as a where a.row_number <=3;
 
+--- Rank :- When using Rank, it will escape somRanking within parition , with gaps and same ranking for tied values 
 
+
+--- Dense Rank :- 
